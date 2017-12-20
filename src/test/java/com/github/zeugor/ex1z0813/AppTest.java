080c63d27c09cc762cc6446c5bd468bb88dc5257
zeugor <zeugor@gmail.com>
2017-12-20 17:04:48 +0100

    maven structure

---
+++ src/test/java/com/github/zeugor/ex1z0813/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.zeugor.ex1z0813;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+  @Test
+  public void test_main() {}
+  
+}
